b878b3c58ba7e5800346fc9f7ccf465c32514041
esteban.araya <esteban.araya@domo.com>
2012-05-18 10:04:18 -0600

    made klassy an amd module

--- src/klassy.js
+++ src/klassy.js
@@ -1,39 +1,42 @@
-var Class = function(parent) {
-	var klass = function() {
-		this.init.apply(this, arguments);
-	};
-	
-	// Subclass
-	if(parent) {
-		var subclass = function () {};
-		// Note: only instance properties get inherited.
-		subclass.prototype = parent.prototype;
-		klass.prototype = new subclass;
+define(function() {
+	var Class = function(parent) {
+		var klass = function() {
+			this.init.apply(this, arguments);
+		};
+		
+		// Subclass
+		if(parent) {
+			var subclass = function () {};
+			// Note: only instance properties get inherited.
+			subclass.prototype = parent.prototype;
+			klass.prototype = new subclass;
+		}
+		
+		klass.prototype.init = function() {};
+		
+		// Shortcut to proto
+		klass.fn = klass.prototype;
+		
+		// Shortcut to class
+		klass.fn.parent = klass;
+		
+		klass._super = klass.__proto__;
+		
+		// Add class or 'static' properties
+		klass.extend = function(obj) {
+			for(var prop in obj) {
+				klass[prop] = obj[prop]
+			}
+		};
+		
+		// Add instance properties
+		klass.include = function(obj) {
+			for(var prop in obj) {
+				klass.fn[prop] = obj[prop];
+			}
+		};
+		
+		return klass;
 	}
-	
-	klass.prototype.init = function() {};
-	
-	// Shortcut to proto
-	klass.fn = klass.prototype;
-	
-	// Shortcut to class
-	klass.fn.parent = klass;
-	
-	klass._super = klass.__proto__;
-	
-	// Add class or 'static' properties
-	klass.extend = function(obj) {
-		for(var prop in obj) {
-			klass[prop] = obj[prop]
-		}
-	};
-	
-	// Add instance properties
-	klass.include = function(obj) {
-		for(var prop in obj) {
-			klass.fn[prop] = obj[prop];
-		}
-	};
-	
-	return klass;
-}
+	return Class;
+});
